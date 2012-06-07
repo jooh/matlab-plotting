e5527bdda5db3650eaaf19a8a6d994653b8b184d
Johan Carlin <johan.carlin@gmail.com>
2012-06-07 14:37:55 +0100

    added alpha support to showimages

--- showimages.m
+++ showimages.m
@@ -1,13 +1,23 @@
 % Basic figure with subplot of images in cell array
-% f = showimages(images);
-function f = showimages(images);
+% f = showimages(images,[alpha]);
+function f = showimages(images,alphadata);
 
 nimages = length(images);
 hw = ceil(sqrt(nimages));
+
+if ieNotDefined('alphadata')
+    hasalpha = 0;
+else
+    hasalpha = 1;
+    assert(length(alphadata)==nimages,'n images does not match n alpha');
+end
 
 f = figurebetter([15 15]);
 
 for n = 1:nimages
     subplot(hw,hw,n);
-    imshow(images{n});
+    ih = imshow(images{n});
+    if hasalpha
+        set(ih,'alphadata',alphadata{n});
+    end
 end
