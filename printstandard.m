c2d361cd297ffa5b2d20639c9f8c639cd3fe28ce
Johan Carlin <johan.carlin@gmail.com>
2012-06-21 19:06:09 +0100

    added printstandard option to force painters renderer

--- printstandard.m
+++ printstandard.m
@@ -2,9 +2,10 @@
 % fnbase - path and filename (with or without extension - we strip)
 % F - figure handle. Optional.
 % r - resolution (dpi). Defaults to 300.
+% forcepainters - always print with painters. Default 1.
 % Dependencies: convert (from ImageMagick)
-% printstandard(fnbase,[F],[r])
-function printstandard(fnbase,F,r);
+% printstandard(fnbase,[F],[r],[forcepainters])
+function printstandard(fnbase,F,r,forcepainters);
 
 if ieNotDefined('F')
   F = gcf;
@@ -12,6 +13,10 @@ end
 
 if ieNotDefined('r')
     r = 300;
+end
+
+if ieNotDefined('forcepainters')
+    forcepainters = 1;
 end
 
 % Make sure no extension
@@ -23,10 +28,16 @@ hand = sprintf('-f%d',F);
 % Ideally 'none' but this is broken as of 2011a
 set(gcf,'color',[1 1 1]);
 
-% Forcing painters means that alpha blending won't work but the output is
-% generally nicer otherwise
-print([fnbase '.png'],hand,'-dpng',res,'-noui','-painters');
-print([fnbase '.eps'],hand,'-depsc','-noui','-painters');
+if forcepainters
+    % Forcing painters means that alpha blending won't work but the output is
+    % generally nicer otherwise
+    print([fnbase '.png'],hand,'-dpng',res,'-noui','-painters');
+    print([fnbase '.eps'],hand,'-depsc','-noui','-painters');
+else
+    print([fnbase '.png'],hand,'-dpng',res,'-noui');
+    print([fnbase '.eps'],hand,'-depsc','-noui');
+end
+
 
 % Trim whitespace
 system(sprintf('convert -density %d %s -trim %s',r,[fnbase '.png'],...
