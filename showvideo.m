e3a57709d8fa5ffe97d1a8fccb97937ba602ff06
Johan Carlin <johan.carlin@gmail.com>
2012-08-09 19:21:34 +0100

    much improved - now opens in new figure, appropriately resized

--- showvideo.m
+++ showvideo.m
@@ -1,7 +1,7 @@
-% Show a video (4D matrix with frame in 4th dim) in the current axis n
-% times (default 1) at fps (default 24).
+% Show a video (4D matrix with frame in 4th dim) in a new figure n times
+% (default 1) at fps (default 24).
 % ax = showvideo(vidmat,[n],[fps])
-function ax = showvideo(vidmat,nrep,fps)
+function showvideo(vidmat,nrep,fps)
 
 if ieNotDefined('nrep')
     nrep = 1;
@@ -12,8 +12,17 @@ if ieNotDefined('fps')
 end
 
 nframes = size(vidmat,4);
+% nb gray has no effect on rgb videos
+cm = gray(256);
 for n = 1:nframes
-    frames(n) = im2frame(vidmat(:,:,:,n));
+    frames(n) = im2frame(vidmat(:,:,:,n),cm);
 end
 
-movie(frames,nrep,fps);
+vidsize = size(vidmat);
+
+F = figurebetter;
+set(F,'units','pixels','position',[0 0 vidsize(1:2)*1.3]);
+axis tight
+axis off
+set(gca,'position',[0 0 1 1]);
+movie(frames,nrep,fps)
