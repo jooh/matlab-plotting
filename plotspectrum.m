ea5b7dead378deaaba3b06d282550b4ad9a666c6
Johan Carlin <johan.carlin@gmail.com>
2013-03-06 19:23:29 +0000

    support for plotting multiple spectra in a matrix at once

--- plotspectrum.m
+++ plotspectrum.m
@@ -1,12 +1,15 @@
-% Plot amplitude spectrum for a 1D signal into current axes.
-% Fs is the sample rate (e.g. 1/TR)
+% Plot amplitude spectrum for into current axes.
+% Y is nobservations by nfeatures (fft along rows, one line plotted per
+% feature).
+% Fs is the sample rate (e.g. 1/TR). Scalar, same for all features.
 % P = plotspectrum(Y,Fs)
 function P = plotspectrum(Y,Fs)
 
-L = length(Y);
+L = size(Y,1);
 NFFT = 2^nextpow2(L);
-fY = fft(Y,NFFT)/L;
+fY = fft(Y,NFFT,1)./L;
 f = Fs/2*linspace(0,1,NFFT/2+1);
-P = plot(f,2*abs(fY(1:NFFT/2+1)))
-xlabel('frequency (Hz)')
-ylabel('amplitude')
+P = plot(f,2.*abs(fY(1:NFFT/2+1,:)));
+xlabel('frequency (Hz)');
+ylabel('amplitude');
+xlim([0 max(f)]);
