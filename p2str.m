0decc28d99503d50ffe7516ba5771101499d70f0
Johan Carlin <johan.carlin@gmail.com>
2013-03-15 16:24:13 +0000

    nice print of p values in char

---
+++ p2str.m
@@ -0,0 +1,12 @@
+% given a scalar or vector of p values, return a cell array of strings with
+% n decimal places (default 3), where p values less than n are rounded to
+% 10^-n.
+% pstr = p2str(p,n)
+function pstr = p2str(pvec,n)
+
+if ieNotDefined('n')
+    n = 3;
+end
+
+pstr = arrayfun(@(p)sprintf(['%.' int2str(n) 'f'],max([p 10^-n])),pvec,...
+    'uniformoutput',false);
