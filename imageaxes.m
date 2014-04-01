981c7d52d5327336a8751c974f982afbadad9767
Johan Carlin <johan.carlin@gmail.com>
2014-04-01 18:29:26 +0100

    refactored to use imshow rather than create hacky axes. Much more robust now.

--- imageaxes.m
+++ imageaxes.m
@@ -1,60 +1,72 @@
-% Generate a set of images at [x,y] locations in current axis.
-% images and alphas are cell arrays. Changes figure units to cm to prevent
-% figure window resizing from breaking the code.
-% imageaxes(ax,xy,images,[alphas],[imsize])
-function outh = imageaxes(ax,xy,images,alphas,imsize)
+% Generate a set of images at [xy] locations in axis ax.  images and alphas
+% are cell arrays. This function will set your dataaspectratio to [1 1 1]
+% to enable square pixels.
+%
+% imheight: height of image in axis units (width gets set appropriately to
+%   preserve aspect ratios). Defaults to 15% of range(ylim)
+%
+% outh = imageaxes(ax,xy,images,[alphas],[imheight])
+function [outh] = imageaxes(ax,xy,images,alphas,imheight)
 
+% input checks and setup
 if ieNotDefined('ax')
     ax = gca;
 end
-axes(ax);
-f = gcf;
-oldfigunits = get(f,'units');
-set(f,'units','centimeters','defaultaxesunits','centimeters');
-set(ax,'units','centimeters','plotboxaspectratio',[1 1 1],...
-    'dataaspectratio',[1 1 1]);
-actualpos = plotboxpos(ax);
-
-% flag for alpha channel
-noalpha = ieNotDefined('alphas');
-
-[n,ndim] = size(xy);
-
-% adaptive scaling by n
-if ieNotDefined('imsize')
-    imsize = 1/(n/3);
+% essential for square pixels
+set(ax,'dataaspectratio',[1 1 1]);
+if ieNotDefined('imheight')
+    % 15% of current y limits
+    imheight = range(ylim)*.15;
 end
 
-% input checks
+[n,ndim] = size(xy);
 assert(ndim==2,'only 2 dimensions are supported!')
-assert(n==length(images),'images and xy are different lengths!')
+assert(n==length(images),'images and xy are different lengths')
+% flag for alpha channel
+hasalpha = ~ieNotDefined('alphas');
+if hasalpha
+    assert(n==length(alphas),'alpha and xy are different lengths');
+end
 
-% imsize as proportion of longest axis
-maxdim = max(actualpos(3:4));
-imsize_sc = imsize * maxdim;
-pad_sc = imsize_sc/5;
+% unfortunately imshow likes to muck about with the plot settings so to
+% stop this...
+axx = get(ax,'xlim');
+axy = get(ax,'ylim');
+axpos = get(ax,'position');
+axstate = get(ax,'visible');
+axdir = get(ax,'ydir');
+axtickdir = get(ax,'tickdir');
 
-% scale XY coordinates into normalised positions
-xyvec = reshape(xy,1,[]);
-xyvec_sc = rescale(xyvec,[pad_sc maxdim*(1-pad_sc/2)]);
-xy_sc = reshape(xyvec_sc,size(xy));
-% add intercept
-xy_final = xy_sc + repmat(actualpos(1:2)-[imsize_sc imsize_sc]/2,[n 1]);
-
-outh = NaN([1 n]);
-for im = 1:n
-    a = axes('position',[xy_final(im,1) xy_final(im,2) imsize_sc imsize_sc],...
-        'plotboxaspectratiomode','auto');
-    % detect BW since this messes up colormap
-    if ndims(images{im}) < 3
-        outh(im) = imshow(repmat(images{im},[1 1 3]));
-    else
-        outh(im) = imshow(images{im});
+if strcmp(axdir,'normal')
+    % need to flip all the images and alphas upside down
+    for im = 1:n
+        images{im} = images{im}(end:-1:1,:,:);
+        if hasalpha
+            alphas{im} = alphas{im}(end:-1:1,:,:);
+        end
     end
-    if ~noalpha
+end
+
+washold = ishold;
+hold on;
+for im = 1:n
+    imsize = size(images{im});
+    aspectratio = imsize(1)/imsize(2);
+    % scale width by ar
+    imwidth = imheight / aspectratio;
+    % offset to centre images rather than place in top left corner
+    xpos = [0 imwidth] - imwidth/2;
+    ypos = [0 imheight] - imheight/2;
+    outh(im) = imshow(images{im},'parent',ax,'initialmagnification','fit',...
+        'xdata',xpos+xy(im,1),'ydata',ypos+xy(im,2));
+    if hasalpha
         set(outh(im),'alphadata',alphas{im});
     end
 end
-% Return focus to master axis
-axes(ax);
-axis off;
+set(outh,'clipping','off');
+% return plot to sanity
+set(ax,'visible',axstate,'position',axpos,'xlim',axx,'ylim',axy,...
+    'ydir',axdir,'tickdir',axtickdir);
+if ~washold
+    hold off;
+end
