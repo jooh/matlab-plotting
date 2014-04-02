abb1bb9253f626b9dafdca4b77a1f50d357d1a75
Johan Carlin <johan.carlin@gmail.com>
2014-04-02 10:13:12 +0100

    new function for setting plot handle Z coordinate - sometimes useful to force correct layer order when uistack fails

---
+++ setplotz.m
@@ -0,0 +1,11 @@
+% set the Z coordinate for the plot hands in phand to some scalar z. This
+% is sometimes useful when uistack fails to make Matlab respect layer
+% order.
+%
+% setplotz(phand,z)
+function setplotz(phand,z)
+
+px = get(phand,'xdata');
+for p = 1:length(phand)
+    set(phand(p),'zdata',repmat(z,[1,numel(px{p})]));
+end
