0496422990184971feb5d993550bd09a5978a6b3
Johan Carlin <johan.carlin@gmail.com>
2015-01-21 17:45:58 +0000

    updated defaults, support for formats input

--- printstandard.m
+++ printstandard.m
@@ -1,13 +1,17 @@
 % Print a figure as PNG and EPS, trim whitespace post-print
 % fnbase - path and filename (with or without extension - we strip)
 % F - figure handle. Optional.
-% r - resolution (dpi). Defaults to 300.
+% r - resolution (dpi). Defaults to 600.
+% formats - print outputs. Defaults to {'png','eps'}.
 % forcepainters - always print with painters. Default 0.
 % Dependencies: convert (from ImageMagick)
 % printstandard(fnbase,varargin)
 function printstandard(fnbase,varargin);
 
-getArgs(varargin,{'F',gcf,'r',600,'forcepainters',0,'loose',false});
+getArgs(varargin,{'F',gcf,'formats',{'png','eps'},...
+    'r',600,'forcepainters',0,'loose',false});
+
+formstruct = struct('png','-dpng','eps','-depsc2');
 
 % Make sure no extension
 fnbase = stripext(fnbase);
@@ -15,7 +19,7 @@ fnbase = stripext(fnbase);
 res = sprintf('-r%d',r);
 hand = sprintf('-f%d',F);
 
-% Ideally 'none' but this is broken as of 2011a
+% Ideally 'none' but this doesn't work with painters
 set(gcf,'color',[1 1 1]);
 
 args = {hand,[],res,'-noui'};
@@ -28,13 +32,17 @@ if loose
     args(end+1) = {'-loose'};
 end
 
-args{2} = '-dpng';
-print([fnbase '.png'],args{:});
-args{2} = '-depsc2';
-print([fnbase '.eps'],args{:});
-%args{2} = '-dpdf';
-%print([fnbase '.pdf'],args{:});
+if ~iscell(formats)
+    formats = {formats};
+end
+for f = formats(:)'
+    fstr = f{1};
+    args{2} = formstruct.(fstr);
+    print([fnbase '.' fstr],args{:});
+end
 
 % Trim whitespace
-%system(sprintf('convert -density %d %s -trim %s',r,[fnbase '.png'],...
-%  [fnbase '.png']));
+if any(strcmp(formats,'png'))
+    system(sprintf('convert -density %d %s -trim %s',r,[fnbase '.png'],...
+      [fnbase '.png']));
+end
