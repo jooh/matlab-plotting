ff8d400e45c7895929e8cbc0e54524a78cb97023
Johan Carlin <johan.carlin@gmail.com>
2015-01-21 17:56:56 +0000

    new call syntax, refactored for 3D support, nan support

--- textscatter.m
+++ textscatter.m
@@ -1,8 +1,7 @@
 % plot scatter where text strings are used as markers into gca.
 %
 % INPUTS:
-% x: coordinates
-% y: coordinates
+% xy: coordinates - can by xy or xyz in columns
 % thand: cell array of strings
 % colors: default [0 0 0]. If size(colors,1) matches numel(x) we plot each
 %   text string (and maybe marker) in the specified color.
@@ -13,70 +12,98 @@
 % thand: handles to text objects
 % p: handles to markers
 %
-% [thand,p] = textscatter(x,y,t,[colors],plotmarker)
-function [thand,p] = textscatter(x,y,t,colors,plotmarker)
+% [thand,p] = textscatter(xy,t,[colors],plotmarker)
+function [thand,p] = textscatter(xy,t,colors,plotmarker)
 
-thand = [];
+% p may be left undefined if ~plotmarker
 p = [];
 
-% skip nans
-nans = isnan(x) | isnan(y);
-x(nans) = [];
-y(nans) = [];
-t(nans) = [];
-n = numel(x);
+% text fails in mysterious ways with single inputs
+xy = double(xy);
 
+% handle single char t inputs
 if ischar(t)
-  t = {t};
+    t = {t};
+end
+switch numel(t)
+    case 1
+        % support plotting the same text label multiple times
+        t = repmat(t,[1,size(xy,1)]);
+    case size(xy,1)
+        % all good
+    otherwise
+        error('numel(t) does not match size(xy,1)');
 end
 
-if numel(t) == 1
-  % support plotting the same text label multiple times
-  t = repmat(t,[1,numel(x)]);
+% skip nans
+nans = any(isnan(xy),2);
+xy(nans,:) = [];
+t(nans) = [];
+n = size(xy,1);
+% number of dimensions to plot over
+nd = size(xy,2);
+
+% track hold state
+changedhold = false;
+if ~ishold
+    hold on;
+    changedhold = true;
+end
+
+% if we are plotting a marker the text needs to shift away from center
+if ieNotDefined('plotmarker')
+    plotmarker = false;
+end
+if plotmarker
+    halign = 'right';
+    valign = 'bottom';
+else
+    halign = 'center';
+    valign = 'middle';
+end
+
+% handle 2D or 3D plots
+switch nd
+    case 2
+        plotter = @(p,co)plot(p(:,1),p(:,2),'.','color',co);
+        texter = @(p,t,co)text(p(:,1),p(:,2),t,'horizontalalignment',...
+            halign,'verticalalignment',valign,'color',co);
+    case 3
+        plotter = @(p,co)plot3(p(:,1),p(:,2),p(:,3),'.','color',co);
+        texter = @(p,t,co)text(p(:,1),p(:,2),p(:,3),t,...
+            'horizontalalignment',halign,'verticalalignment',valign,...
+            'color',co);
+    otherwise
+        error('unsupported input dimensionality: %d',nd);
 end
 
 if ieNotDefined('colors')
-  colors = [0 0 0];
+    colors = [0 0 0];
 elseif size(colors,1)>1
-  colors(nans,:) = [];
+    colors(nans,:) = [];
 end
 
-if ieNotDefined('plotmarker')
-  plotmarker = false;
+if ~plotmarker
+    % invisible plot to get axis limits set properly
+    p = plotter(xy,[1 1 1]);
+    set(p,'linestyle','none','marker','none');
 end
 
-changedhold = false;
-if ~ishold
-  hold on;
-  changedhold = true;
-end
-
-if plotmarker
-  halign = 'right';
-  valign = 'bottom';
-else
-  % invisible plot to get axis limits set properly
-  p = plot(x,y,'marker','none','linestyle','none');
-  halign = 'center';
-  valign = 'middle';
-end
-
-thand = text(x,y,t,'horizontalalignment',halign,'verticalalignment',valign);
 if size(colors,1)==1
-  set(thand,'color',colors);
-  if plotmarker
-    p = plot(x,y,'.','color',colors);
-  end
-else
-  for ind = 1:n
-    set(thand(ind),'color',colors(ind,:));
+    % one call will do
+    thand = texter(xy,t,colors);
     if plotmarker
-      p(ind) = plot(x(ind),y(ind),'.','color',colors(ind,:));
+        p = plotter(xy,colors);
     end
-  end
+else
+    for ind = 1:n
+        thand(ind) = texter(xy(ind,:),t(ind),colors(ind,:));
+        if plotmarker
+            p(ind) = plotter(xy(ind,:),colors(ind,:));
+        end
+    end
 end
 
 if changedhold
-  hold off;
+    hold off;
 end
-
