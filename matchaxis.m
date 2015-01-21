a850ffdea38397fd8258fd2f55898ff358865554
Johan Carlin <johan.carlin@gmail.com>
2015-01-21 17:42:35 +0000

    use axis handle

--- matchaxis.m
+++ matchaxis.m
@@ -1,10 +1,12 @@
 % given an array of axis handles, set the data limits and the plot boxes to
 % match.
-% matchaxis(ax)
-function matchaxis(ax)
+% [matchedax,matchedpos] = matchaxis(ax)
+function [newax, newpos] = matchaxis(ax)
 
 if numel(ax) < 2
   % nothing to match
+  newax = axis(ax);
+  newpos = get(ax,'position');
   return
 end
 
@@ -14,10 +16,12 @@ nd = size(axmat,2);
 newmins = min(axmat(:,[1:2:nd]),[],1);
 newmaxs = max(axmat(:,[2:2:nd]),[],1);
 newax = [newmins; newmaxs];
-axis(ax,newax(:)');
+newax = newax(:)';
+axis(ax,newax);
 
 % match axis height/width 
 apos = cell2mat(get(ax,'position'));
 newsize = min(apos(:,3:4),[],1);
 newpos = [apos(:,1:2) repmat(newsize,[length(ax),1])];
 arrayfun(@(x)set(ax(x),'position',newpos(x,:)),1:length(ax));
+newpos = newpos(1,:);
