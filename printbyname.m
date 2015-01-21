5f37d110fe4c2d2347df2505ec2f5b47b7dc7c78
Johan Carlin <johan.carlin@gmail.com>
2015-01-21 17:45:33 +0000

    documentation

--- printbyname.m
+++ printbyname.m
@@ -1,3 +1,13 @@
+% print figures by reading file names from the 'name' field (which must be
+% set before calling this). bad characters get replaced by underscores in
+% file names.
+%
+% INPUT
+% fighand: handles to figures
+% outdir: directory to print to
+% varargin: any additional arguments for printstandard
+%
+% printbyname(fighand,outdir,varargin)
 function printbyname(fighand,outdir,varargin)
 
 mkdirifneeded(outdir);
