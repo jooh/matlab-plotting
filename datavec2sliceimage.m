99eb3e798f34515bb5524b839c4f7f5c799cb37e
Johan Carlin <johan.carlin@gmail.com>
2015-01-21 17:37:35 +0000

    support non-binary inputs

--- datavec2sliceimage.m
+++ datavec2sliceimage.m
@@ -1,8 +1,8 @@
 % convenient wrapper for 
-% makeimagestack(datavec2mat(datavec,mask),varargin{:});
+% makeimagestack(datavec2mat(datavec,mask~=0),varargin{:});
 %
 % ih = datavec2sliceimage(datavec,mask,varargin)
 function ih = datavec2sliceimage(datavec,mask,varargin)
 
-mat = datavec2mat(datavec,mask);
+mat = datavec2mat(datavec,mask~=0);
 ih = makeimagestack(mat,varargin{:});
