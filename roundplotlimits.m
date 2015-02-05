4cf496c3dfb0fd990467b2b82104819e9afc00c7
Johan Carlin <johan.carlin@gmail.com>
2015-02-05 17:28:01 +0000

    new function for rounding plot limits up to some precision for nice labels

---
+++ roundplotlimits.m
@@ -0,0 +1,42 @@
+% expand the current plot limits to the nearest value with a given
+% precision (default 1 decimal point).
+%
+% ax            gca     axis handle
+% axdir         'y'     axis to process ('xy' is also supported)
+% specialvalue  NaN     if present and the limits are outside this range we
+%                           rescale to include it
+% precision     1       number of decimal points
+% 
+% roundplotlimits(ax,axdir,specialvalue,precision)
+function roundplotlimits(ax,axdir,specialvalue,precision)
+
+if ieNotDefined('ax')
+    ax = gca;
+end
+
+if ieNotDefined('axdir')
+    axdir = 'y';
+end
+
+if ieNotDefined('precision')
+    precision = 1;
+end
+
+if ieNotDefined('specialvalue')
+    specialvalue = NaN;
+end
+
+for thisax = ax(:)'
+    for thisdir = axdir(:)'
+        lim = get(thisax,[thisdir 'lim']);
+        lim = [reduceprecision(lim(1),precision,@floor) ...
+            reduceprecision(lim(2),precision,@ceil)];
+        if lim(2)<specialvalue
+            lim(2) = specialvalue;
+        end
+        if lim(1)>specialvalue
+            lim(1) = specialvalue;
+        end
+        set(thisax,[thisdir 'lim'],lim);
+    end
+end
