1c91ad0d7756bef24184e98041f4dc0b3eaa3381
Johan Carlin <johan.carlin@gmail.com>
2015-04-30 15:37:56 +0100

    refactors to better handle non-integer grids

--- imageplot.m
+++ imageplot.m
@@ -3,37 +3,80 @@
 % neighbour interpolation, which helps get around an unfixed Matlab bug
 % that produces blurry EPS output on Macs.
 %
-% imageplot(ax,im,varargin)
-function imageplot(ax,im,varargin)
+% INPUT:
+% ax            gca         axis handle
+% im            -           RGB image
+%
+% rowlabels     []          for imageticks
+% collabels     []          for imageticks
+% nrows         2
+% rotatelabels  45
+% gridcolor     [1 1 1]     t
+% gridlines     []
+% ticklines     0
+% upscale       'auto'
+% outline       []
+% outlinecolor  'm'
+%
+% ih = imageplot(ax,im,varargin)
+function ih = imageplot(ax,im,varargin)
 
+nrow = size(im,1);
+ncol = size(im,2);
 getArgs(varargin,{'rowlabels',[],'collabels',[],'nrows',2,...
     'rotatelabels',45,'gridcolor',[1 1 1],'gridlines',[],...
-    'ticklines',0,'upscale','auto'});
+    'ticklines',0,'upscale','auto','outline',[],'outlinecolor','m',...
+    'xlims',[1 ncol],'ylims',[1 nrow]});
 
-ncon = size(im,1);
 
 minsize = 500;
-if strcmp(upscale,'auto') && ncon<minsize
+minn = min([nrow ncol]);
+if strcmp(upscale,'auto')
     % find a multiplication by 8 that gets us over 500 pixels
-    upscale = ceil(minsize / ncon / 8) * 8;
+    upscale = ceil(minsize / minn / 8) * 8;
 end
 
 if upscale>1
-    im = imresize(im,upscale,'nearest');
+    % we could use imresize with 'nearest' but this reduces the licensing
+    % requirements...
+    im = im(repinds(1,nrow,upscale),repinds(1,ncol,upscale),:);
 end
 
-% upscale but preserve the overall positioning (each entry centered on
-% integers 1:n)
-pixr = (ncon)/size(im,1)/2;
-% +pixr because the width of the pixel takes care of drawing the rest
-startpoint = .5+pixr;
-% -pixr ...
-endpoint = ncon+.5-pixr;
-ih = image([startpoint endpoint],[startpoint endpoint],im,'parent',ax);
-lims = [.5 ncon+.5];
-ticks = 1:ncon;
-set(ax,'xlim',lims,'ylim',lims,'xtick',ticks,'ytick',ticks,...
-    'tickdir','out');
+% here the .5 modifiers are probably an issue. 
+
+% I think we need to take 1 off here to allow for the extra span (the first
+% pixel needs an extra half pixelwidth to the left and the last an extra
+% half to the right)
+pixheight = range(ylims) / (nrow-1);
+pixwidth = range(xlims) / (ncol-1);
+% so this tells us how much padding is needed around the sides to avoid
+% clipping off the first and final half pixel.
+
+% width of each actual pixel after upscaling
+pixwidth_up = pixwidth / upscale;
+pixheight_up = pixheight / upscale;
+
+% so here's what we do
+% 1) find the left edge where we want to start drawing
+% (xlims(1)-pixwidth/2)
+% 2) increment at each upscaled pixel width
+% 3) finish at the RIGHT edge where we want to end (xlims(2)+pixwidth/2)
+% 4) shift this array by half an upscaled pixel to center
+allx = [xlims(1)-pixwidth/2:pixwidth_up:xlims(2)+pixwidth/2] + ...
+    pixwidth_up/2;
+% 5) drop off the final pixel that goes outside the range (this is covered
+% by the centering)
+allx(end) = [];
+
+ally = [ylims(1)-pixheight/2:pixheight_up:ylims(2)+pixheight/2] + ...
+    pixheight_up/2;
+% drop off overshoot
+ally(end) = [];
+
+ih = image(allx,ally,im,'parent',ax);
+set(ax,'xlim',xlims + pixwidth./[-2 2],'ylim',ylims + pixheight./[-2 2],...
+    'xtick',xlims(1):pixwidth:xlims(2),...
+    'ytick',ylims(1):pixheight:ylims(2),'tickdir','out');
 
 % for display only dataaspectratio needs to be set, but by also setting
 % plotboxaspectratio we obtain more accurate estimates of axis position
@@ -42,10 +85,13 @@ set(ax,'dataaspectratio',[1 1 1],'plotboxaspectratio',[1 1 1]);
 
 % sort out x/y labels
 if isempty(rowlabels) && isempty(collabels)
-    set(ax,'xticklabel',ticks,'yticklabel',ticks);
     axis(ax,'off');
 elseif isnumeric(rowlabels) || ischar(rowlabels) || (iscell(rowlabels) && ischar(rowlabels{1}))
     % text labels
+    assert(numel(collabels)==numel(get(ax,'xtick')),...
+        'number of collabels does not match number of xtick');
+    assert(numel(rowlabels)==numel(get(ax,'ytick')),...
+        'number of rowlabels does not match number of ytick');
     set(ax,'xticklabel',collabels,'yticklabel',rowlabels);
     if ~isempty(rotatelabels) && rotatelabels~=0
         rotateXLabels(ax,rotatelabels);
@@ -60,9 +106,15 @@ end
 box(ax,'off');
 
 if ~isempty(gridlines)
-    % remove any gridlines exceeding ncon (can happen e.g. when collapsing
+    % remove any gridlines exceeding nrow (can happen e.g. when collapsing
     % split RDMs)
-    gridlines(gridlines>ncon) = [];
-    [x,y] = meshgrid(gridlines+.5,[.5 ncon+.5]);
+    gridlines(gridlines>nrow) = [];
+    [x,y] = meshgrid(gridlines+.5,[.5 nrow+.5]);
     gridh = line([x y],[y x],'linewidth',.5,'color',gridcolor);
 end
+
+if ~isempty(outline)
+    outhand = imoutline(outline,'color',outlinecolor,...
+        'linewidth',1);
+end
+
