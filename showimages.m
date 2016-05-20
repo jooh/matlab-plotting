91aca13acfb6072a755d904587147c77f8cbce64
Johan Carlin <johan.carlin@gmail.com>
2016-05-20 16:48:51 +0100

    return axis handles with showimages

--- showimages.m
+++ showimages.m
@@ -4,8 +4,8 @@
 % you have a specific shape in mind. plotfun is a handle to substitute
 % imshow (e.g. for plotting intensity images with imagesc).
 %
-% f = showimages(images,[alpha],[dims],[plotfun],[donumbers]);
-function f = showimages(images,alphadata,dims,plotfun,donumbers);
+% [f,ax] = showimages(images,[alpha],[dims],[plotfun],[donumbers]);
+function [f,ax]= showimages(images,alphadata,dims,plotfun,donumbers);
 
 % Support calling with a struct array (fields image, alpha)
 if (isstruct(images) || isobject(images))
@@ -40,8 +40,9 @@ end
 
 f = figurebetter([15 15]);
 
+ax = NaN([1,nimages]);
 for n = 1:nimages
-    subplot(dims(1),dims(2),n);
+    ax(n) = subplot(dims(1),dims(2),n);
     ih = feval(plotfun,images{n});
     if hasalpha
         set(ih,'alphadata',alphadata{n});
