94a7c2d3e69696633722a017a8acc186205dec36
Johan Carlin <johan.carlin@gmail.com>
2017-05-02 12:22:54 +0100

    return names

--- readimages.m
+++ readimages.m
@@ -1,13 +1,18 @@
 % convenience function for reading all images found by the output of some
 % dir command into a cell array. Optionally returns the alpha layers as a
-% second output
-% [images,alphas] = readimages(dircmd)
-function [images,alphas] = readimages(dircmd)
+% second output, and the names (file names without extension) as a third
+% [images,alphas,names] = readimages(dircmd)
+function [images,alphas,names] = readimages(dircmd)
 
 files = dir(dircmd);
 [root,fn,ext] = fileparts(dircmd);
 nfiles = length(files);
 assert(nfiles>0,'no files found for command %s',dircmd);
 for f = 1:length(files)
-    [images{f},junk,alphas{f}] = imread(fullfile(root,files(f).name));
+    [~,names{f},~] = fileparts(files(f).name);
+    [images{f},~,alphas{f}] = imread(fullfile(root,files(f).name));
+    % fill in an all-on alpha channel
+    if isempty(alphas{f})
+        alphas{f} = ones(asrow(size(images{f}),2));
+    end
 end
