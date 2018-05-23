8085657ee31c2fdddd8f11ea9e75fb4e5f6cf5ff
Johan Carlin <johan.carlin@gmail.com>
2018-05-23 13:51:02 +0100

    support 3D/4D matrix inputs, documentation, handle alpha channel more robustly

--- showimages.m
+++ showimages.m
@@ -1,20 +1,39 @@
-% Basic figure with subplot of images in cell array
-% call either with cell arrays images and optionally alpha, or with a
-% struct array with image and alpha fields. dims gives rows and columns if
-% you have a specific shape in mind. plotfun is a handle to substitute
-% imshow (e.g. for plotting intensity images with imagesc).
+% Basic figure with subplot of images in cell array / struct / matrix
+%
+% INPUT     DEFAULT     DEFINITION
+% images        -                       cell array, struct arrage (with image
+%                                           and possibly alpha fields), matrix
+%                                           (3D or 4D with image index along
+%                                           final dim)
+% alpha         []                      alpha layer (cell array)
+% dims          ceil(sqrt(nimages))     subplot dimensions
+% plotfun       @image                  function to call to make plots. @imagesc
+%                                           is a good alternative.
+% donumbers     true                    show image number in title field
 %
 % [f,ax] = showimages(images,[alpha],[dims],[plotfun],[donumbers]);
 function [f,ax]= showimages(images,alphadata,dims,plotfun,donumbers);
 
 % Support calling with a struct array (fields image, alpha)
 if (isstruct(images) || isobject(images))
-    try 
+    if isfield(images,'alpha')
         alphadata = {images.alpha};
-    catch
-        alphadata = [];
     end
     images = {images.image};
+elseif isnumeric(images)
+    imsize = size(images);
+    switch ndims(images)
+        case {2,3}
+            % assume you stacked images in third dim
+            images = mat2cell(images,imsize(1),imsize(2),ones(1,imsize(3)));
+        case 4
+            % assume 4th dim
+            assert(imsize(3)==3,...
+                'input matrix images must have 3 entries along third dim (RGB)');
+            images = mat2cell(images,imsize(1),imsize(2),imsize(3),ones(1,imsize(4)));
+        otherwise
+            error('unknown dimensionality for input images: %d',ndims(images));
+    end
 end
 
 nimages = length(images);
